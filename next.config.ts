8ff880795f2d28c3f5021d801f0c2684a53967b9
Dallas <dallas.huggins@deptagency.com>
2025-01-28 13:07:19 -0500

    Env variables

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    MAILERLITE_NEWSLETTER_GROUP: process.env.MAILERLITE_NEWSLETTER_GROUP,
+    MAILERLITE_API_KEY: process.env.MAILERLITE_API_KEY,
+  },
 };
 
 export default nextConfig;
